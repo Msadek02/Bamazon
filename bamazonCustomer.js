9c5f0deb09adf4e7d7efdc713d9d82792050ae13
Msadek02 <Thunder_axe_90@hotmail.com>
2018-03-11 17:42:55 -0400

    displaying all items for sale

---
+++ bamazonCustomer.js
@@ -0,0 +1,30 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+const cTable = require('console.table');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "Bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  afterConnection();
+});
+
+function afterConnection() {
+    var query = "SELECT * FROM products";
+  connection.query(query , function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    connection.end();
+  });
+}
