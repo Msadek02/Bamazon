2faa3dd03155b9d323b4c3814be4f18c4e7c6fa0
Msadek02 <Thunder_axe_90@hotmail.com>
2018-03-14 15:10:57 -0400

    add inventory manager app

--- bamazonManager.js
+++ bamazonManager.js
@@ -93,6 +93,79 @@ function displayLowInventory() {
     })
 }
 
+function validateInteger(value) {
+    var integer = Number.isInteger(parseFloat(value));
+    var sign = Math.sign(value);
+
+    if (integer && (sign === 1)) {
+        return true;
+    } else {
+        return 'Please enter a whole non-zero number.';
+    }
+}
+
+function validateNumber(value) {
+    var number = (typeof parseFloat(value)) === 'number';
+	var positive = parseFloat(value) > 0;
+
+    if (number && positive) {
+        return true;
+    } else {
+        return 'Please enter a whole non-zero number.';
+    }
+}
+
+function addInventory() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'item_id',
+            message: 'Please enter the Item ID for stock_count update',
+            validate: validateInteger,
+            filter: Number
+        },
+        {
+            type: 'input',
+            name: 'quantity',
+            message: 'How many would you like to add?',
+            validate:validateInteger,
+            filter: Number
+        }
+    ]).then(function(input) {
+
+        var item = input.item_id;
+        var addQuantity = input.quantity;
+
+        var query = 'SELECT * FROM products WHERE ?';
+
+        connection.query(query, {item_id: item}, function(err, data) {
+            if (err) throw err;
+
+            if (data.length === 0) {
+                console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+                addInventory();
+
+            } else {
+                var productData = data[0];
+
+                console.log('Updating Inventory......');
+
+                var updateQuery = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity + addQuantity) + ' WHERE item_id = ' + item;
+
+                connection.query(updateQuery, function(err, res) {
+                    if (err) throw err;
+
+                    console.log('Stock count for Item ID ' + item + ' has been updated to ' + (productData.stock_quantity + addQuantity) + '.');
+                    console.log("\n---------------------------------------------------------------------\n");
+                    
+
+                    connection.end();
+                })
+            }
+        })
+    })
+}
+
 function runBamazon() {
 
 	// Prompt manager for input
